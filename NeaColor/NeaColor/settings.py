a210b51f9069646dbb5090e10311199d0d22e502
Guillermo <nicomano021@gmail.com>
2022-03-11 09:43:57 -0300

    asd

--- NeaColor/NeaColor/settings.py
+++ NeaColor/NeaColor/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
             'ENGINE': 'django.db.backends.mysql',
             'NAME': 'nea',
             'USER': 'root',
-            'PASSWORD': '357159',
+            'PASSWORD': 'root',
             'HOST': 'localhost',
             'PORT': '3306',
         }
